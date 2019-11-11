9678d4509786f70a53d64f96b699544f42c4981e
kbethany <apemi@mail.ru>
2019-11-11 19:59:02 +0300

    fixes ldi

--- vm/ldi_sti.c
+++ vm/ldi_sti.c
@@ -39,6 +39,24 @@ int reg_dir(unsigned char	arg_byte, int num)
 	return (arg);
 }
 
+void	print_4bytes(int a1, int a2, int a3, int flag_l)
+{
+	if (flag_l)
+	{
+		g_arena->list[g_bogies->index + a1 + a2].com = a3 / 16777216;
+		g_arena->list[g_bogies->index + 1 + a1 + a2].com = (a3 % 16777216) / 65536;
+		g_arena->list[g_bogies->index + 2 + a1 + a2].com = (a3 % 65536) / 256;
+		g_arena->list[g_bogies->index + 3 + a1 + a2].com = a3 % 256;
+	}
+	else
+	{
+		g_arena->list[g_bogies->index + (a1 + a2) % IDX_MOD].com = a3 / 16777216;
+		g_arena->list[g_bogies->index + 1 + (a1 + a2) % IDX_MOD].com = (a3 % 16777216) / 65536;
+		g_arena->list[g_bogies->index + 2 + (a1 + a2) % IDX_MOD].com = (a3 % 65536) / 256;
+		g_arena->list[g_bogies->index + 3 + (a1 + a2) % IDX_MOD].com = a3 % 256;
+	}
+}
+
 void ldi(void)
 {
 	int				arg_1;
@@ -59,11 +77,9 @@ void ldi(void)
 	arg_2 = reg_dir(arg_byte, SECOND_ARG);
 	arg_3 = get_tdir_small_size(g_bogies->index + g_bogies->aim);
 	g_bogies->aim++;
-	g_arena->list[g_bogies->index + (arg_1 + arg_2) % IDX_MOD].com = arg_3 / 16777216;
-	g_arena->list[g_bogies->index + 1 + (arg_1 + arg_2) % IDX_MOD].com = (arg_3 % 16777216) / 65536;
-	g_arena->list[g_bogies->index + 2 + (arg_1 + arg_2) % IDX_MOD].com = (arg_3 % 65536) / 256;
-	g_arena->list[g_bogies->index + 3 + (arg_1 + arg_2) % IDX_MOD].com = arg_3 % 256;
+	print_4bytes(arg_1, arg_2, arg_3, 0);
 	move_caret(g_bogies->aim);
+	g_bogies->aim = 0;
 }
 
 void sti(void)
@@ -82,14 +98,12 @@ void sti(void)
 	}
 	arg_byte = g_arena->list[g_bogies->index + 1].com;
 	arg_1 = get_tdir_small_size(g_bogies->index + 2);
-	g_bogies->aim = 3;
+	g_bogies->aim = 2;
 	arg_2 = all_three(arg_byte, SECOND_ARG);
 	arg_3 = reg_dir(arg_byte, THIRD_ARG);
-	g_arena->list[g_bogies->index + (arg_3+ arg_2) % IDX_MOD].com = arg_1 / 16777216;
-	g_arena->list[g_bogies->index + 1 + (arg_3 + arg_2) % IDX_MOD].com = (arg_1 % 16777216) / 65536;
-	g_arena->list[g_bogies->index + 2 + (arg_3 + arg_2) % IDX_MOD].com = (arg_1 % 65536) / 256;
-	g_arena->list[g_bogies->index + 3 + (arg_3 + arg_2) % IDX_MOD].com = arg_1 % 256;
+	print_4bytes(arg_2, arg_3, arg_1, 0);
 	move_caret(g_bogies->aim);
+	g_bogies->aim = 0;
 }
 
 void lldi(void)
@@ -114,9 +128,7 @@ void lldi(void)
 	arg_2 = reg_dir(arg_byte, SECOND_ARG);
 	arg_3 = get_tdir_small_size(g_bogies->index + g_bogies->aim);
 	g_bogies->aim++;
-	g_arena->list[g_bogies->index + arg_1 + arg_2].com = arg_3 / 16777216;
-	g_arena->list[g_bogies->index + 1 + arg_1 + arg_2].com = (arg_3 % 16777216) / 65536;
-	g_arena->list[g_bogies->index + 2 + arg_1 + arg_2].com = (arg_3 % 65536) / 256;
-	g_arena->list[g_bogies->index + 3 + arg_1 + arg_2].com = arg_3 % 256;
+	print_4bytes(arg_1, arg_2, arg_3, 1);
 	move_caret(g_bogies->aim);
+	g_bogies->aim = 0;
 }
