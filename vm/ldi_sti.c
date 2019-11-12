ccb1287cfcc559094d61fa51d211f9eb9029f1f0
kbethany <apemi@mail.ru>
2019-11-12 15:01:14 +0300

    sti fix

--- vm/ldi_sti.c
+++ vm/ldi_sti.c
@@ -6,7 +6,7 @@ int	all_three(unsigned char	arg_byte, int num)
 
 	if (IS_T_REG(arg_byte, num))
 	{
-		arg = get_treg(g_bogies->aim);
+		arg = g_bogies->regs[get_treg(g_bogies->aim) - 1];
 		g_bogies->aim++;
 	}
 	else if (IS_T_DIR(arg_byte, num))
@@ -41,20 +41,16 @@ int reg_dir(unsigned char	arg_byte, int num)
 
 void	print_4bytes(int a1, int a2, int a3, int flag_l)
 {
+	int c;
 	if (flag_l)
-	{
-		g_arena->list[g_bogies->index + a1 + a2].com = a3 / 16777216;
-		g_arena->list[g_bogies->index + 1 + a1 + a2].com = (a3 % 16777216) / 65536;
-		g_arena->list[g_bogies->index + 2 + a1 + a2].com = (a3 % 65536) / 256;
-		g_arena->list[g_bogies->index + 3 + a1 + a2].com = a3 % 256;
-	}
+		c =  a1 + a2;
 	else
-	{
-		g_arena->list[g_bogies->index + (a1 + a2) % IDX_MOD].com = a3 / 16777216;
-		g_arena->list[g_bogies->index + 1 + (a1 + a2) % IDX_MOD].com = (a3 % 16777216) / 65536;
-		g_arena->list[g_bogies->index + 2 + (a1 + a2) % IDX_MOD].com = (a3 % 65536) / 256;
-		g_arena->list[g_bogies->index + 3 + (a1 + a2) % IDX_MOD].com = a3 % 256;
-	}
+		c = (a1 + a2) % IDX_MOD;
+	a3 = g_bogies->regs[a3];
+	g_arena->list[g_bogies->index + c].com = a3 >> 24;
+	g_arena->list[g_bogies->index + 1 + c].com = (a3 << 8) >> 24;
+	g_arena->list[g_bogies->index + 2 + c].com = (a3 << 16) >> 24;
+	g_arena->list[g_bogies->index + 3 + c].com = (a3 << 24) >> 24;
 }
 
 void ldi(void)
@@ -84,26 +80,23 @@ void ldi(void)
 
 void sti(void)
 {
-	int				arg_1;
-	int				arg_2;
-	int				arg_3;
-	unsigned char	arg_byte;
+	int				arg_1 = 0;
+	int				arg_2 = 0;
+	int				arg_3 = 0;
+	unsigned char	arg_byte = 0;
 
-	if (g_bogies->its_a_highnoon != g_arena->round)
-		return ;
 	if (IS_T_IND(arg_byte, THIRD_ARG) || IS_T_IND(arg_byte, FIRST_ARG) || IS_T_DIR(arg_byte, FIRST_ARG))
 	{
 		skip_bytes(STI_OP);
 		return ;
 	}
 	arg_byte = g_arena->list[g_bogies->index + 1].com;
-	arg_1 = get_tdir_small_size(g_bogies->index + 2);
-	g_bogies->aim = 2;
+	arg_1 = get_treg(2) - 1;
+	g_bogies->aim = 3;
 	arg_2 = all_three(arg_byte, SECOND_ARG);
 	arg_3 = reg_dir(arg_byte, THIRD_ARG);
 	print_4bytes(arg_2, arg_3, arg_1, 0);
-	move_caret(g_bogies->aim);
-	g_bogies->aim = 0;
+	move_caret(g_bogies->aim - 1);
 }
 
 void lldi(void)
