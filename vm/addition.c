dfadf80ac014fc07882c7e04614423cfba26eca0
GremGrog <gremmsta@gmail.com>
2019-11-28 11:53:06 +0300

    norminette addition.c

--- vm/addition.c
+++ vm/addition.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   addition.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmasha-h <fmasha-h@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/11/28 11:43:19 by fmasha-h          #+#    #+#             */
+/*   Updated: 2019/11/28 11:52:10 by fmasha-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "vm.h"
 
 void	addition(void)
@@ -9,26 +21,18 @@ void	addition(void)
 
 	arg_byte = g_arena->list[(g_bogies->index + 1) % MEM_SIZE].com;
 	if (is_treg(arg_byte, FIRST_ARG) && is_treg(arg_byte, SECOND_ARG) &&
-		is_treg(arg_byte, THIRD_ARG))
+		is_treg(arg_byte, THIRD_ARG)
+		&& ((reg1 = get_treg(2)) < 16)
+		&& ((reg2 = get_treg(3)) < 16)
+		&& ((reg3 = get_treg(4)) < 16))
 	{
-		reg1 = get_treg(2);
-		reg2 = get_treg(3);
-		reg3 = get_treg(4);
-		if (reg1 >= 16 || reg2 >= 16 || reg3 >= 16)
-		{
-			skip_bytes(ADD_OP);
-			return ;
-		}
 		g_bogies->regs[reg3] = g_bogies->regs[reg1] + \
 												g_bogies->regs[reg2];
-		if (g_bogies->regs[reg3] == 0)
-			g_bogies->carry = 1;
-		else
-			g_bogies->carry = 0;
-		g_bogies->aim = 4;
+		g_bogies->carry = (g_bogies->regs[reg3] == 0 ? 1 : 0);
 		if (g_flags->v == 1 || g_flags->v == 30)
-			ft_printf("P %4d | add r%d r%d r%d\n", g_bogies->num, reg1 + 1, reg2 + 1, reg3 + 1);
-		move_caret(g_bogies->aim + 1);
+			ft_printf("P %4d | add r%d r%d r%d\n", g_bogies->num, \
+										reg1 + 1, reg2 + 1, reg3 + 1);
+		move_caret(5);
 	}
 	else
 	{
