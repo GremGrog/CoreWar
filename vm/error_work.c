e770d630c28d0214c7e653649d3090ce78d45631
kbethany <apemi@mail.ru>
2019-11-29 17:11:07 +0300

    usage

--- vm/error_work.c
+++ vm/error_work.c
@@ -6,16 +6,32 @@
 /*   By: fmasha-h <fmasha-h@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/28 13:12:20 by fmasha-h          #+#    #+#             */
-/*   Updated: 2019/11/28 14:19:32 by fmasha-h         ###   ########.fr       */
+/*   Updated: 2019/11/29 17:09:43 by kbethany         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
+void	usage_print(void)
+{
+	ft_printf(2, "usage: ./corewar [-i] [-dump N] [-n N] [-a] [-v N]
+	example.cor ...\n-i				: visualisator\n
+	-dump N		: Dumps memory after N cycles then exits\n
+	-n N			: set the champion number\n
+	-a				: Print output from \"aff\"\n
+	-v N			: verbose\n
+						- 1		:	Show opearions information\n
+						- 2		: Show cycles information\n
+						- 3		: Show lives information\n
+						- 4		: Show deaths information\n
+						- 5		: Show PC movement information\n
+						- 30	: Show all information\n");
+}
+
 int		ft_errno(int x)
 {
 	if (x == 0)
-		ft_fprintf(2, "error: no argument\n");
+		usage_print();
 	if (x == 1)
 		ft_fprintf(2, "error: wrong number after flag -n\n");
 	if (x == 2)
